9f1ab08ec0e7a1fbd4f9dcc20a44efffef8d4af7
Mati Andreas <mati.andreas@ambientia.ee>
2020-06-02 09:16:47 +0300

    step 2

--- cdk/lib/constructs/sns-topics.ts
+++ cdk/lib/constructs/sns-topics.ts
@@ -3,7 +3,7 @@ import * as CDK from '@aws-cdk/core';
 import {env} from "../../bin/env";
 
 export type PossibleSnsTopics = {
-    SNS_TOPIC_ERRORS: SNS.ITopic;
+    SNS_TOPIC_ERRORS?: SNS.ITopic;
 };
 
 export type SnsTopicNames = keyof PossibleSnsTopics;
@@ -14,18 +14,19 @@ export type AllSnsTopics = {
 
 // TODO Step 2.1: Define topic Name here - should be one defined in AllSnsTopics
 
-// const createTopic: (stack: CDK.Stack, id: string) => SNS.ITopic =
-//     (stack, id) => {
-//         const topicName = `${id}-${env}`;
-//
-//         return new SNS.Topic(stack, topicName, {
-//             topicName,
-//         });
-//     };
+const createTopic: (stack: CDK.Stack, id: string) => SNS.ITopic =
+    (stack, id) => {
+        const topicName = `${id}-${env}`;
+
+        return new SNS.Topic(stack, topicName, {
+            topicName,
+        });
+    };
 
 // TODO: Step 2.1. Use this to create all topics!
 export const createTopics: (stack: CDK.Stack) => AllSnsTopics =
-    () => {
+    (stack) => {
         return {
+            SNS_TOPIC_ERRORS: createTopic(stack, `errors`)
         };
     };
